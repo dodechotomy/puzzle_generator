70a1dafb528a4f4503c4d902ac337940b9801551
Scott Wilson <me.scottwilson@gmail.com>
2018-03-13 19:32:37 -0400

    Fixed duplicate selection of connection points

--- js/sketch.js
+++ js/sketch.js
@@ -1,9 +1,10 @@
 var start = 0;
 var rings = [];
 var thickness = 50;
-var sectors = 4;
+var sectors = 6;
 var pathCount = 2;
-const INNER = 0, OUTER = 1;
+const INNER = 0,
+  OUTER = 1;
 
 function setup() {
   createCanvas(window.innerWidth, window.innerHeight, SVG); // Create SVG Canvas
@@ -38,7 +39,12 @@ function createRings() {
   for (var i = 0; i < 4; i++) {
     var c = createVector(width / 2, height / 2);
     var innerRadius = thickness * (i + 1);
-    var r = makeRing(c, innerRadius, thickness, sectors, sectors);
+    var innerConnections = sectors;
+    if (r) {
+      innerConnections = r.outerConnections;
+    }
+    var outerConnections = sectors;
+    var r = makeRing(c, innerRadius, thickness, innerConnections, outerConnections);
     r.innerRing = prevRing;
     if (prevRing) {
       prevRing.outerRing = r;
@@ -48,59 +54,97 @@ function createRings() {
   }
   for (var j = 0; j < pathCount; j++) {
     var r = rings[0];
-    var start = randomConnector(r.connections, true, INNER);
-    var end = randomConnector(r.connections, true, OUTER);
-    var s = makeSpline(r, start, end);
-    r.splines.push(s);
+    var innerConns = filterConnectors(r, true, INNER);
+    var outerConns = filterConnectors(r, true, OUTER);
+    var start = randomItem(innerConns);
+    var end = randomItem(outerConns);
+    makeSpline(r, start, end, (j+1)/(pathCount+1));
+    // r.splines.push(s);
   }
-  for (var i = 1; i < rings.length; i++) {
-    for (var j = 0; j < array.length; j++) {
-      array[j]
+  for(var i = 1; i < rings.length;i++){
+    for(var j =0; j < rings[i-1].splines.length;j++){
+      var s = rings[i-1].splines[j];
+      var start = s.end;
+      var outerConns = filterConnectors(rings[i], true, OUTER);
+      var end = randomItem(outerConns);
+      makeSpline(rings[i], start, end, s.level);
     }
   }
 }
 
-function makeRing(center, innerRadius, thickness, innerConnections, outerConnectionCount) {
-  var ring = {};
-  ring.center = center;
-  ring.innerRadius = innerRadius;
-  ring.thickness = thickness;
-  // ring.middleRadius = ring.innerRadius + (thickness / 2);
-  ring.outerRadius = ring.innerRadius + thickness;
-  ring.splines = [];
-  ring.innerRing = null;
-  ring.outerRing = null;
-  ring.connections = innerConnections;
-  var outerConnections = [];
-  for (var i = 0; i < ring.outerConnectionCount; i++) {
-    var position = p5.Vector.fromAngle(TWO_PI*i/ring.outerConnectionCount).mult(ring.outerRadius).add(ring.center);
-    outerConnections.outer.push({
-      index: i,
-      position: position,
-      splines: []
-    });
+function makeRing(center, innerRadius, thickness, innerConnections = sectors, outerConnections = sectors) {
+  var ring = {
+    center: center,
+    innerRadius: innerRadius,
+    thickness: thickness,
+    outerRadius: innerRadius + thickness,
+    splines: [],
+    innerRing: null,
+    outerRing: null,
+    innerConnections: [],
+    outerConnections: []
+  };
+  if (typeof innerConnections === 'number') {
+    for (var i = 0; i < innerConnections; i++) {
+      var position = p5.Vector.fromAngle(TWO_PI * i / innerConnections).mult(ring.innerRadius)
+      ring.innerConnections.push({
+        index: i,
+        position: position,
+        splines: []
+      });
+    }
+  } else if (typeof innerConnections === 'array') {
+    ring.innerConnections = innerConnections;
+  }
+  if (typeof outerConnections === 'number') {
+    for (var i = 0; i < outerConnections; i++) {
+      var position = p5.Vector.fromAngle(TWO_PI * i / outerConnections).mult(ring.outerRadius);
+      ring.outerConnections.push({
+        index: i,
+        position: position,
+        splines: []
+      });
+    }
+  } else if (typeof outerConnections === 'array') {
+    ring.outerConnections = outerConnections;
   }
   return ring;
 }
 
+function getConnections(ring) {
+  return concat(ring.innerConnections, ring.outerConnections);
+}
+
+function randomItem(array) {
+  var rand = random();
+  return array[Math.floor(rand * array.length)];
+}
+
 function drawRing(ring) {
+  push();
+  translate(ring.center.x, ring.center.y);
   stroke('#ED225D');
-  ellipse(ring.center.x, ring.center.y, ring.innerRadius * 2, ring.innerRadius * 2);
-  ellipse(ring.center.x, ring.center.y, ring.outerRadius * 2, ring.outerRadius * 2);
+  ellipse(0, 0, ring.innerRadius * 2, ring.innerRadius * 2);
+  ellipse(0, 0, ring.outerRadius * 2, ring.outerRadius * 2);
   for (var i = 0; i < ring.splines.length; i++) {
     drawSpline(ring.splines[i]);
   }
+  pop();
 }
 
-function randomConnector(list, free, side) {
-  filtered = list;
-  if(typeof free === 'boolean'){
-    filtered = filtered.filter(conn => (conn.target == null) == free)
+function filterConnectors(ring, free, side) {
+  var list = getConnections(ring);
+  if (side === INNER) {
+    list = ring.innerConnections;
   }
-  if(typeof side === 'number'){
-    filtered = filtered.filter(conn => conn.side == side)
+  if (side === OUTER) {
+    list = ring.outerConnections;
   }
-  return list[floor(random(list.length))];
+
+  if (typeof free === 'boolean') {
+    list = list.filter(conn => (conn.target == null) == free)
+  }
+  return Array.from(list);
 }
 
 function makeSpline(ring, conn1, conn2, level = 0.5) {
@@ -117,28 +161,21 @@ function makeSpline(ring, conn1, conn2, level = 0.5) {
 
 function drawSpline(spline) {
   var ring = spline.ring;
-  var innerAngle = spline.inner / ring.innerDivisions * TWO_PI;
-  var outerAngle = spline.outer / ring.outerDivisions * TWO_PI;
+  var innerAngle = spline.start.position.heading();
+  var outerAngle = spline.end.position.heading();
+  if(outerAngle<innerAngle){
+    var t = outerAngle;
+    outerAngle = innerAngle;
+    innerAngle = t;
+  }
   var middleRadius = ring.innerRadius + (ring.outerRadius - ring.innerRadius) * spline.level;
-  var points = [];
-  points.push(p5.Vector.fromAngle(innerAngle).mult(ring.innerRadius));
-  points.push(p5.Vector.fromAngle(innerAngle).mult(middleRadius));
-  points.push(p5.Vector.fromAngle(outerAngle).mult(middleRadius));
-  points.push(p5.Vector.fromAngle(outerAngle).mult(ring.outerRadius));
-  points.forEach(function(p) {
-    p.add(ring.center);
-  })
-  strokeWeight(6);
-  stroke("white");
-  line(points[0].x, points[0].y, points[1].x, points[1].y);
-  arc(ring.center.x, ring.center.y, middleRadius * 2, middleRadius * 2, innerAngle, outerAngle);
-  line(points[2].x, points[2].y, points[3].x, points[3].y);
-
+  var arc1 = spline.start.position.copy().setMag(middleRadius);
+  var arc2 = spline.end.position.copy().setMag(middleRadius);
   strokeWeight(2);
   stroke("blue");
-  line(points[0].x, points[0].y, points[1].x, points[1].y);
-  arc(ring.center.x, ring.center.y, middleRadius * 2, middleRadius * 2, innerAngle, outerAngle);
-  line(points[2].x, points[2].y, points[3].x, points[3].y);
+  line(spline.start.position.x, spline.start.position.y, arc1.x, arc1.y);
+  arc(0, 0, middleRadius * 2, middleRadius * 2, innerAngle, outerAngle);
+  line(arc2.x, arc2.y, spline.end.position.x, spline.end.position.y);
 }
 
 function useConnection(ring, conn) {
